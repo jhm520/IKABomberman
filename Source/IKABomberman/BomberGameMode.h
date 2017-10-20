fc7932265fca465ad5e52ea6db7318ede3935577
jhm520 <jhm520@gmail.com>
2017-10-20 11:43:14 -0700

    Initial Build Commit
    Added 1 C++ class to generate solution.

---
+++ Source/IKABomberman/BomberGameMode.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BomberGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class IKABOMBERMAN_API ABomberGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/IKABomberman/IKABomberman.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "IKABomberman.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, IKABomberman, "IKABomberman" );
