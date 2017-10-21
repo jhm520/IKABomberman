bb2b3547415068c19890e9de195021f0f4b413a1
jhm520 <jhm520@gmail.com>
2017-10-21 16:37:18 -0700

    Map, materials, player pawn movement.
    Added player pawn movement in C++.

--- Source/IKABomberman/BomberCharacter.cpp
+++ Source/IKABomberman/BomberCharacter.cpp
@@ -3,11 +3,24 @@
 #include "BomberCharacter.h"
 
 
+
+namespace MovementInputVectors
+{
+	const FVector Up(1, 0, 0);
+	const FVector Down(-1, 0, 0);
+	const FVector Left(0, -1, 0);
+	const FVector Right(0, 1, 0);
+}
 // Sets default values
 ABomberCharacter::ABomberCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	InputMap.Add(EMovementInputEnum::Up, false);
+	InputMap.Add(EMovementInputEnum::Down, false);
+	InputMap.Add(EMovementInputEnum::Left, false);
+	InputMap.Add(EMovementInputEnum::Right, false);
 
 }
 
@@ -18,11 +31,109 @@ void ABomberCharacter::BeginPlay()
 	
 }
 
+void ABomberCharacter::SetInput(const EMovementInputEnum InInputKey, const bool bPressed)
+{
+	bool* FoundInput = InputMap.Find(InInputKey);
+
+	const bool InputIsPressed = *FoundInput;
+
+	if (FoundInput && InputIsPressed != bPressed)
+	{
+		*FoundInput = bPressed;
+
+		if (bPressed)
+		{
+
+			switch (InInputKey)
+			{
+				case EMovementInputEnum::Up:
+					InputVector += MovementInputVectors::Up;
+					break;
+				case EMovementInputEnum::Down:
+					InputVector += MovementInputVectors::Down;
+					break;
+				case EMovementInputEnum::Left:
+					InputVector += MovementInputVectors::Left;
+					break;
+				case EMovementInputEnum::Right:
+					InputVector += MovementInputVectors::Right;
+					break;
+			}
+			/*MovementSwitch = InInputKey;
+			MovementKeysPressedNum++;*/
+		}
+		else
+		{
+			switch (InInputKey)
+			{
+				case EMovementInputEnum::Up:
+					InputVector -= MovementInputVectors::Up;
+					break;
+				case EMovementInputEnum::Down:
+					InputVector -= MovementInputVectors::Down;
+					break;
+				case EMovementInputEnum::Left:
+					InputVector -= MovementInputVectors::Left;
+					break;
+				case EMovementInputEnum::Right:
+					InputVector -= MovementInputVectors::Right;
+					break;
+			}
+
+			/*if (MovementKeysPressedNum > 0)
+			{
+				MovementKeysPressedNum--;
+			}
+			
+			if (MovementKeysPressedNum == 0)
+			{
+				MovementSwitch = EMovementInputEnum::None;
+				return;
+			}
+			else
+			{
+				for (TPair<EMovementInputEnum, bool> InputPair : InputMap)
+				{
+					if (InputPair.Value)
+					{
+						MovementSwitch = InputPair.Key;
+						break;
+					}
+				}
+			}*/
+		}
+	}
+}
+
+void ABomberCharacter::TickMovement()
+{
+	AddMovementInput(InputVector, 1.0f);
+
+	/*switch (MovementSwitch)
+	{
+		case EMovementInputEnum::None:
+			break;
+		case EMovementInputEnum::Up:
+			AddMovementInput(MovementInputVectors::Up, 1.0f);
+			break;
+		case EMovementInputEnum::Down:
+			AddMovementInput(MovementInputVectors::Down, 1.0f);
+			break;
+		case EMovementInputEnum::Left:
+			AddMovementInput(MovementInputVectors::Left, 1.0f);
+			break;
+		case EMovementInputEnum::Right:
+			AddMovementInput(MovementInputVectors::Right, 1.0f);
+			break;
+	}*/
+}
+
 // Called every frame
 void ABomberCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	TickMovement();
 }
 
 // Called to bind functionality to input
--- Source/IKABomberman/BomberCharacter.h
+++ Source/IKABomberman/BomberCharacter.h
@@ -6,6 +6,16 @@
 #include "GameFramework/Character.h"
 #include "BomberCharacter.generated.h"
 
+UENUM(BlueprintType)
+enum class EMovementInputEnum : uint8
+{
+	None	UMETA(DisplayName = "None"),
+	Up		UMETA(DisplayName = "Up"),
+	Down	UMETA(DisplayName = "Down"),
+	Left	UMETA(DisplayName = "Left"),
+	Right	UMETA(DisplayName = "Right")
+};
+
 UCLASS()
 class IKABOMBERMAN_API ABomberCharacter : public ACharacter
 {
@@ -19,13 +29,26 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	EMovementInputEnum MovementSwitch;
+
+	FVector InputVector = FVector(0, 0, 0);
+
+	UFUNCTION(BlueprintCallable, Category = Movement)
+	void SetInput(const EMovementInputEnum InInputKey, const bool bPressed);
+
+
+	TMap<EMovementInputEnum, bool> InputMap;
+
+	int8 MovementKeysPressedNum = 0;
+
+
+	void TickMovement();
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
-	
 	
 };
